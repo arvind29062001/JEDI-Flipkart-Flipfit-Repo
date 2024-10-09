e620f5a745465e317105b01db87de68e349be2d4
shakti.singh3 <shakti.singh3@fk1089-224781l.fkipl.flipkart.com>
2024-10-09 12:09:41 +0530

    Some Beans added(booking ,gym,owner,slot,person)

---
+++ JEDI-Flipfit-Java-POS/src/com/flipfit/bean/FlipfitGymOwner.java
@@ -0,0 +1,41 @@
+package com.flipfit.bean;
+
+public class FlipfitGymOwner extends Person{
+
+
+   private String uniqueIdProof;
+   private String gstNumber;
+
+
+   // Getter & Setter
+   public String getUniqueIdProof() {
+      return uniqueIdProof;
+   }
+
+   public void setUniqueIdProof(String uniqueIdProof) {
+      this.uniqueIdProof = uniqueIdProof;
+   }
+
+   public String getGstNumber() {
+      return gstNumber;
+   }
+
+   public void setGstNumber(String gstNumber) {
+      this.gstNumber = gstNumber;
+   }
+
+
+   //Method
+
+   public void registerGym(){
+
+   }
+
+   public void updateSlot(){
+
+
+   }
+
+
+
+}
